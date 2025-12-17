d8e9cbfda8a3e046333c9521b5e685d4cf2b8f99
rindraniaina <rindraniainarasolofondrasoa17@gmail.com>
2025-12-17 14:09:12 +0300

    feat: dish and ingredients class

---
+++ src/main/java/com/school/hei/CategoryEnum.java
@@ -0,0 +1,5 @@
+package com.school.hei;
+
+public enum CategoryEnum {
+    VEGETABLE, ANIMAL, MARINE, DAIRY, OTHER
+}
---
+++ src/main/java/com/school/hei/DishTypeEnum.java
@@ -0,0 +1,5 @@
+package com.school.hei;
+
+public enum DishTypeEnum {
+    START, MAIN, DESSERT
+}
